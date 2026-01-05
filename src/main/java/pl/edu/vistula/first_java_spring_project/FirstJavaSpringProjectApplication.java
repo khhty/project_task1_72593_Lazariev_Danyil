b34880968f49c96deff9c8b65fd8a3afc5832b2c
Danyil Lazariev <daniyllaz@gmail.com>
2026-01-05 12:44:08 +0100

    task1

---
+++ src/main/java/pl/edu/vistula/first_java_spring_project/FirstJavaSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpringProjectApplication.class, args);
+	}
+
+}
